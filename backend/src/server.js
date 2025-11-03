42b550b8d13e44229b8f5a970dbcb266be0ade62
pierreyouvape <pierre.youvape@gmail.com>
2025-11-03 18:51:51 +0100

    augmentation payload 200mb

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,8 +21,8 @@ const PORT = process.env.BACKEND_PORT || 3000;
 
 // Middlewares
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ limit: '200mb', extended: true }));
 
 // Routes
 app.get('/api/health', (req, res) => {
